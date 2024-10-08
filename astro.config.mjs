3116499e0246356e6e631dda2417ea5954d84216
Mardoqueo Zotelo <mardoflorentin18@gmail.com>
2024-10-07 21:19:42 -0300

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import tailwind from '@astrojs/tailwind';
 
 import vercel from '@astrojs/vercel/serverless';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: vercel()
+  adapter: netlify()
 });
