4419c567f823469fa7426dede0fdd767d9d4bded
MardoqueoZ <mardoflorentin18@gmail.com>
2025-03-03 12:55:35 -0300

    cv nuevo

--- .netlify/build/entry.mjs
+++ .netlify/build/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
-import { manifest } from './manifest_BD5axlGE.mjs';
+import { manifest } from './manifest_DJU6l8Nm.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
@@ -18,7 +18,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "0e6a2d8e-d884-4e91-a2e6-70fca7d733c7"
+    "middlewareSecret": "346f60e5-4302-4197-9e6e-0d4e1e5ab234"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
