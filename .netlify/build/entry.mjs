936b63b41f075f17c499f41f0d943999de84ba24
MardoqueoZ <mardoflorentin18@gmail.com>
2025-03-09 22:40:18 -0300

    ajuste de tags

--- .netlify/build/entry.mjs
+++ .netlify/build/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_CvSoi7hX.mjs';
-import { manifest } from './manifest_DJU6l8Nm.mjs';
+import { manifest } from './manifest_CklyMOhc.mjs';
 import { createExports } from '@astrojs/netlify/ssr-function.js';
 
 const _page0 = () => import('./pages/_image.astro.mjs');
@@ -18,7 +18,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "346f60e5-4302-4197-9e6e-0d4e1e5ab234"
+    "middlewareSecret": "6079017d-c567-4a93-9cb7-fcd8b03fcf9e"
 };
 const _exports = createExports(_manifest, _args);
 const __astrojsSsrVirtualEntry = _exports.default;
